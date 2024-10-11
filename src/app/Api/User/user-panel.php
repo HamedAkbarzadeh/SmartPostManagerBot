fa420f05c00508886c1e7f89de26b65297e0e00f
hamed <h.akbarzadh@gmail.com>
2024-10-11 06:30:51 -0700

    change : change keyboard in user-panel.php

--- src/app/Api/User/user-panel.php
+++ src/app/Api/User/user-panel.php
@@ -43,10 +43,7 @@ if (strpos($telegramApi->getText(), '/start') === 0) {
                     ['text' => 'sendPost_5']
                 ]
             ],
-            'resize_keyboard' => true,
-            'one_time_keyboard' => true
         ];
-
     } else {
         $text = "به ربات " . BOT_NAME . " خوش آمدید . جهت ورود به کانال به لینک زیر مراجعه نمایید" . PHP_EOL . CHANNEL_USERNAME;
     }
